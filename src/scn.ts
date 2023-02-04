c5029afa977b6ad59cf2d0e766870101497389ea
Fayez Nazzal <soulldev@gmail.com>
2023-02-04 08:36:09 +0200

    Use return types

--- src/scn.ts
+++ src/scn.ts
@@ -6,7 +6,7 @@
  * ### Output:
  * "text-sm text-success"
  */
-export = (...cn: ([string, any] | (string | undefined | null | boolean))[]) =>
+export = (...cn: ([string, any] | (string | undefined | null | boolean))[]): string =>
   cn
     .filter((x) => (x as any)?.[1])
     .map((x) => ((x as Array<any>).pop ? (x as Array<any>)[0] : x))
